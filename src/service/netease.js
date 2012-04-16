bdc154d58965f48c23e5b6651d1eb36bcb8f545b
popomore <sakura9515@gmail.com>
2012-04-16 16:38:56 +0800

    netease sohu支持before

--- src/service/netease.js
+++ src/service/netease.js
@@ -1,5 +1,8 @@
 sharejs.pushService('netease',{
-	'apiUrl': 'http://t.163.com/article/user/checkLogin.do',
-	'title': 'info',
-	'url': 'link'
+    'apiUrl': 'http://t.163.com/article/user/checkLogin.do',
+    'title': 'info'
 });
+
+sharejs.before('netease',function(option, service){
+    option['title'] = option['title'] + ' ' + option['url']; 
+})
--- src/service/sohu.js
+++ src/service/sohu.js
@@ -1,5 +1,9 @@
 sharejs.pushService('sohu',{
-	'apiUrl': 'http://t.sohu.com/third/post.jsp',
-	'title': 'title',
-	'url': 'url'
+    'apiUrl': 'http://t.sohu.com/third/post.jsp',
+    'title': 'title',
+    'url': 'url'
 });
+
+sharejs.before('sohu',function(option, service){
+    option['content'] = 'utf-8';
+})
